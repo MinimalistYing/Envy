3f2f0c929a1c64967f6f12212fa419c64e7b4686
MinimalistYing <genius_ying@163.com>
2021-06-17 10:05:43 +0800

    react-redux

--- react-redux-demo/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const { removeModuleScopePlugin, addBabelPreset, override } = require('customize-cra')
-
-module.exports = override(
-  removeModuleScopePlugin(),
-  addBabelPreset('@babel/preset-react')
-)
